441cd25b9a8d20647380364991722e5abe3edd83
12157591+AplUSAndmINUS@users.noreply.github.com <terence.waters@imail.org>
2020-04-01 19:44:39 -0600

    initial commit

---
+++ index.tsx
@@ -0,0 +1,6 @@
+/*** index.tsx ***/
+import { AppRegistry } from 'react-native';
+import App from './src/main';
+AppRegistry.registerComponent('GroceriesList', () => App);
+
+AppRegistry.registerComponent(appName, () => App);
